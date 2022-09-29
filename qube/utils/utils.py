02b5bd58631f5287a91c04005a03fb8011779671
dmitry <dmitry.ema@gmail.com>
2022-09-29 14:25:24 +0300

    - Controller properties

--- qube/utils/utils.py
+++ qube/utils/utils.py
@@ -185,7 +185,7 @@ def is_localhost(host):
 
 def _get_remote_host_info(host: str):
     sc_props = Properties.get_main_properties()['services-controller']
-    host = Properties.get_main_properties()['host_alias'].get(host.lower(), host)
+    host = Properties.get_main_properties()['host_aliases'].get(host.lower(), host)
     return host, sc_props['port']
 
 
