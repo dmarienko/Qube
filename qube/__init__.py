9cabf1371ca7f3eb66a9cf2ad74ea94e5586d3f6
dmitry <dmitry.ema@gmail.com>
2022-10-19 18:28:42 +0300

    - Version inc

--- qube/__init__.py
+++ qube/__init__.py
@@ -3,7 +3,7 @@ from . import charting
 from . import datasource
 from . import utils
 
-__version__ = '0.1.9905'
+__version__ = '0.1.9906'
 
 from qube.utils.utils import runtime_env
 from cycler import cycler
