b27f77fe4b6675d369a65a87528cba4da370af4c
dmitry <dmitry.ema@gmail.com>
2022-12-19 18:22:11 +0200

    Added config-test for cases when we need test strategies

--- qube/__init__.py
+++ qube/__init__.py
@@ -3,7 +3,7 @@ from . import charting
 from . import datasource
 from . import utils
 
-__version__ = '0.2.001'
+__version__ = '0.2.002'
 
 from qube.utils.utils import runtime_env
 from cycler import cycler
