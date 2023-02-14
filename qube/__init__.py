1a3753afdacd2d684c26dac9134946c815cbbd0f
dmitry <dmitry.ema@gmail.com>
2023-02-14 16:56:34 +0200

    Fixes version number

--- qube/__init__.py
+++ qube/__init__.py
@@ -3,7 +3,7 @@ from . import charting
 from . import datasource
 from . import utils
 
-__version__ = '0.2.007'
+__version__ = '0.2.008'
 
 from qube.utils.utils import runtime_env
 from cycler import cycler
