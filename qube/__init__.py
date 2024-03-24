2f321eef1f128ddc09e7e65958c1975e1c34b544
dmitry <dmitry.ema@gmail.com>
2024-03-24 15:44:14 +0200

    fix: version increase

--- qube/__init__.py
+++ qube/__init__.py
@@ -3,7 +3,7 @@ from . import charting
 from . import datasource
 from . import utils
 
-__version__ = '0.3.33'
+__version__ = '0.3.34'
 
 from qube.utils.utils import runtime_env
 from cycler import cycler
